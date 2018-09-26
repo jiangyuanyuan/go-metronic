711eef9327075d0187acc14709b259c24fd1ec84
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-09-26 14:39:08 +0800

    第一个GO的index页面

--- controller/IndexController/index.go
+++ controller/IndexController/index.go
@@ -1,7 +1,13 @@
 package IndexController
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+)
 
 type IndexController struct {
 	beego.Controller
+}
+
+func (p *IndexController) Index() {
+	p.TplName = "index/index.html"
 }
--- main/main.go
+++ main/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	_ "workspace/go-metronic/router"
+)
 
 func main() {
 	beego.Run()
